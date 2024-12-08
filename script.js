4650e35a42de1c674a1120eb5e36eb7772e62149
Abdulkabeer555 <akgct123@gmail.com>
2024-12-08 22:29:35 +0500

    js assignment

---
+++ script.js
@@ -0,0 +1,19 @@
+var sum1 = parseFloat(prompt('Enter Number1'));
+var sum2 = parseFloat(prompt('Enter Number2'));
+var add = sum1 + sum2
+alert(`The add of ${sum1} and ${sum2} is ${add}`)
+console.log(`The addition of ${sum1} and ${sum2} is ${add}`)
+
+
+var name = prompt('What is your name?');
+var purchaseAmount = parseFloat(prompt('Enter Your Purchase Amount'));
+var discountAmount = (parseFloat(purchaseAmount)*10)/100;
+var finalAmount = parseFloat(purchaseAmount) - discountAmount;
+console.log(`Name: ${name}
+Your bill after discount: ${finalAmount}`)
+
+
+var animal = (prompt('Your Favourite animal'));
+console.log(`Wow, ${animal} is an amazing creature!`)
+    
+
